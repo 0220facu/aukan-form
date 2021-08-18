efaf1ec19c3e73859a32d3cfd22a7f916dee3192
martin <martin@seemple.com.ar>
2021-08-18 19:28:11 -0300

    simplified html code and added jquery validation

--- aukan re-maquetado/js/main.js
+++ aukan re-maquetado/js/main.js
@@ -1,31 +1,47 @@
 $( document ).ready(function() {
-  var window_width = $(window).width();
-  
-  if(window_width < 900){
-    $('#main-text').removeClass('col-7')
-    $('#main-text').addClass('col-12 container')
 
-    $('#form-container').removeClass('col-5')
-    $('#form-container').addClass('row')
-  }
-}
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
-    
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
+    $( "#sib-form" ).validate({
+      rules: {
+          EMAIL: {
+            required: true,
+            email: true
+          },
+          PHONE: {
+            required:true,
+            minlength:10,
+            number:true
+          },
+          FIRSTNAME:{
+            required:true,
+            minlength:10
           }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-  }))
+      },
+      messages: {
+          EMAIL:{
+              required: "Por favor ingrese su e-mail.",
+              email: "Ingrese su e-mail en el formato correcto."
+          },
+          PHONE:{
+            required: "Por favor ingrese su telefono.",
+            number: "Ingrese sólo números.",
+            minlength: "Debe ingresar al menos 10 caracteres."
+          },
+          FIRSTNAME:{
+            required: "Por favor ingrese su nombre y apellido.",
+            minlength: "Debe ingresar al menos 10 caracteres."
+        }
+      },
+      errorElement: 'span',
+      errorPlacement: function (error, element) {
+          error.addClass('invalid-feedback');
+          element.closest('.form-group').append(error);
+      },
+      highlight: function (element, errorClass, validClass) {
+          $(element).addClass('is-invalid');
+      },
+      unhighlight: function (element, errorClass, validClass) {
+          $(element).removeClass('is-invalid');
+      }
+  });
+
+});
